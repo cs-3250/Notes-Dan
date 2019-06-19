10b886120a935bb4785cc7163fcc5449e166dfe7
Daniel L'Episcopo <24749502+DanLepiscopo@users.noreply.github.com>
2019-06-18 22:41:21 -0600

    Add files via upload

---
+++ Python Programs/Basic Python Concepts.py
@@ -0,0 +1,61 @@
+# Messing around with vairables and floats 
+print(8/2)
+print(16 * 2.5)
+msg = "hello"
+print(msg)
+print("""""")
+
+# Exponentiation - Raising of one number to the power of another
+print(4**3)
+print(7**(1/3))
+print("""""")
+
+# ***Quotient & Remainder***
+
+# Floor Division - Returns how many times a number can go into another
+print(30 // 3)
+print("""""")
+# Modulo - Returns the remainder of a divided problem
+print(27 % 4)
+print("""""")
+
+# Strings - In Python you can use '' or "" for strings
+print('I am a coding God')
+print("Just kidding lol")
+print("""""")
+
+# User Input 
+s = input("Enter your favorite food: ")
+print("User said: " + s)
+print("""""")
+
+# Strings can be multiplied by integers (can't be multiplied by floats)
+print("Cows " * 3)
+print("""""")
+
+
+# TYPE CONVERSION (Casting in Java)
+# We can add two ints together via type converstion
+print(int("2") + int("6"))
+print("""""")
+
+# VARIABLES 
+x = 12
+pickels = 2.5
+print(x + pickels)
+print(x * pickels)
+print(x % pickels)
+print("""""")
+# You are allowed to delete variables as well (pretty cool)
+dogs = "Dogs are Amazing!"
+del dogs
+print(dogs)
+print("""""")
+
+# IN-PLACE OPERATORS 
+# (Python doesn't have ++ like Java)
+# so in order to increment you must use +=
+z = 5
+z += 2
+print(z)
+print("""""")
